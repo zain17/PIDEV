c3353769c4d7f797cf90747dcbe6d5e17a7ec70c
amineamri <amri.amine.1@esprit.tn>
2018-02-19 19:55:40 +0100

    Changement reflettants la creation du BlogBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new EntiteBundle\EntiteBundle(),
+            new BlogBundle\BlogBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
