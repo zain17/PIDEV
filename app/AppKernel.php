1993ecbcf1a9ad8d9d2da04b6d302377aa2544da
amineamri <amri.amine.1@esprit.tn>
2018-02-20 21:11:18 +0100

    Configuration BlogBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new EntiteBundle\EntiteBundle(),
             new BlogBundle\BlogBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
