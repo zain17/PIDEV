66168a5166c85802119136adbbe65b2370713081
zain17 <zainelabidine.bensaleh@esprit.tn>
2018-02-21 20:39:20 +0100

    Photo Entity ADDED

---
+++ src/EntiteBundle/Entity/Photo.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace EntiteBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Photo
+ *
+ * @ORM\Table(name="photo")
+ * @ORM\Entity(repositoryClass="EntiteBundle\Repository\PhotoRepository")
+ */
+class Photo
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
