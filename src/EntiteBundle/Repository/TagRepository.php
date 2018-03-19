4cadd779468d8556ac70f47d10d64b2e66c32025
Zain <zainelabidine.bensaleh@esprit.tn>
2018-03-19 17:03:18 +0100

    Merge remote-tracking branch 'origin/master' into XXXXXXXXXXXXXXR

---
+++ src/EntiteBundle/Repository/TagRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aminos
+ * Date: 2/22/18
+ * Time: 10:49 PM
+ */
+
+namespace EntiteBundle\Repository;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class TagRepository extends EntityRepository
+{
+
+}
